e6fc50abcf7b148188081bd6aeed31a4cffe5983
koijuthathip123 <sisterday.koii@gmail.com>
2024-03-02 12:04:55 +0700

    update db connect

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'demo',
+    'password' => 'demo',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
